46079b9b402b6bcc4c85862497d2df7b26fa107e
Mohammad <zia.ro@live.com>
2022-08-19 18:12:52 -0400

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding"
+Student.create!(first_name: 'Amanda', last_name: 'Bludworth', grade: 10)
+puts "Done seeding"
